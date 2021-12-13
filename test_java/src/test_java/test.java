588b712031df894ee847ea0b546095a5792145d1
g82sk <g82sk@222.110.49.39>
2021-12-13 18:17:20 +0900

    ver 0.2

--- test_java/src/test_java/test.java
+++ test_java/src/test_java/test.java
@@ -3,6 +3,7 @@ package test_java;
 public class test {
 	public static void main(String[] args) {
 		System.out.println("hello git world!");
-		System.out.println("ver : 1.0");
+		System.out.println("ver : 2.0");
+		System.out.println("로그인 함");
 	}
 }
